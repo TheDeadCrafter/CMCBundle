14cd48a83eb20e1ded69d60189d19bcda9d2f240
TheDeadCrafter <schelcc@pobox.com>
2017-01-17 11:22:44 -0500

    Add files via upload

--- CMC Bundle/src/com/classicsmc/src/chat/ChatFilter.java
+++ CMC Bundle/src/com/classicsmc/src/chat/ChatFilter.java
@@ -1,92 +1,34 @@
 package com.classicsmc.src.chat;
 
+import java.util.ArrayList;
+
 import org.bukkit.ChatColor;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.Plugin;
 
 public class ChatFilter {
 	
 	String feedBackMessage = ChatColor.RED + "Please be respectful of other players!";
 	
+	private String[] blockedWords;
+	
+	public void loadConfiguration() {
+		
+		
+	}
+	
 	public void handleLanguage(AsyncPlayerChatEvent e) {
-		if(e.getMessage().contains("ez")) {
+		
+		
+		for (int i = 0; i < blockedWords.length; i++) {
+			String curWord = blockedWords[i];
 			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("goty")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("ezpz")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("fuck")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("shit")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("dick")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("pussy")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("jerk")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("retard")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("cock")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains(" ass ")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if (e.getMessage().contains("hack")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
-		} else if(e.getMessage().contains("hax")) {
-			
-			e.getPlayer().sendMessage(feedBackMessage);
-			
-			e.setCancelled(true);
-			
+			if (e.getMessage().contains(curWord)) {
+				e.getPlayer().sendMessage(feedBackMessage);
+			}
 		}
+		
+
 		
 	}
 	
